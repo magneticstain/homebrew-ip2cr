97ebfd2ba2c675cdfd7041c71ddd94ae21046537
Josh Carlson <magneticstain@gmail.com>
2023-08-15 21:18:50 -0400

    Fix brew audit errors

--- Formula/ip2cr.rb
+++ Formula/ip2cr.rb
@@ -1,8 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ip2cr < Formula
-  desc "IP-2-CloudResource: a tool used for correlating a cloud IP address with its associated resources, with a focus on speed and efficiency."
+  desc "IP-2-CloudResource: a tool for correlating an IP address with its associated resource"
   homepage "https://github.com/magneticstain/ip2cr"
   url "https://github.com/magneticstain/ip2cr/releases/download/v0.0.3/ip2cr_Darwin_x86_64_v0.0.3.tar.gz"
   sha256 "eca4e745e6343e93b906a132084b43903c34a7fd643d68a939c7ed2168a4ac2e"
@@ -11,7 +8,6 @@ class Ip2cr < Formula
   depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
